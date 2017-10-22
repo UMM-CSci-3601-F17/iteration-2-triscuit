b3ee138866b1fc7a19872318e26784b4225da593
M1NN <score@morris.umn.edu>
2017-10-22 13:28:33 -0500

    Add routes to home-page and updated home-page ui

--- server/src/main/java/umm3601/Server.java
+++ server/src/main/java/umm3601/Server.java
@@ -16,7 +16,7 @@ import static spark.Spark.*;
 import static spark.debug.DebugScreen.enableDebugScreen;
 
 public class Server {
-    private static final String databaseName = "i1-droptable-dev";
+    private static final String databaseName = "i2-triscuit-dev";
     private static final int serverPort = 4567;
 
     public static void main(String[] args) throws IOException {
